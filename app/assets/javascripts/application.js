40b1c091e3287c026e3a584d29ec39c6ce1559b9
Patricia <yunayena@gmail.com>
2016-04-03 15:36:39 +0200

    Añadimos front del panel de administración (big commit).

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,27 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require bootstrap.min
+//= require plugins/easypiechart/jquery.easypiechart
+//= require plugins/sparklines/jquery.sparklines.min
+//= require plugins/jstree/dist/jstree.min
+//= require plugins/codeprettifier/prettify
+//= require plugins/bootstrap-switch/bootstrap-switch
+//= require plugins/bootstrap-tabdrop/js/bootstrap-tabdrop
+//= require plugins/iCheck/icheck.min
+//= require enquire.min
+//= require plugins/bootbox/bootbox
+//= require plugins/simpleWeather/jquery.simpleWeather.min
+//= require plugins/nanoScroller/js/jquery.nanoscroller.min
+//= require plugins/jquery-mousewheel/jquery.mousewheel.min
+//= require functions
+//= require plugins/fullcalendar/fullcalendar.min
+//= require plugins/wijets/wijets
+//= require plugins/charts-chartistjs/chartist.min
+//= require plugins/charts-chartistjs/chartist-plugin-tooltip
+//= require plugins/form-daterangepicker/moment.min
+//= require plugins/form-daterangepicker/daterangepicker
+
+$(document).ready(function() {
+
+});
